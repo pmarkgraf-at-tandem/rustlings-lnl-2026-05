c6c87170b24e97e190ecf8f64bb2f8b6a560e6a7
phil.markgraf <pmarkgraf@tandemdiabetes.com>
2026-05-20 13:24:36 -0700

    Week 2 changes

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,13 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(num_apples: i32) -> i32 {
+    if num_apples > 40 {
+        num_apples
+    } else {
+        2* num_apples
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
