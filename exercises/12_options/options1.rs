e02628157615a935bced18aeefc9e88941d95b78
phil.markgraf <pmarkgraf@tandemdiabetes.com>
2026-07-01 14:01:37 -0700

    Work from Week 7

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -4,10 +4,24 @@
 // `hour_of_day` is higher than 23.
 fn maybe_ice_cream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    // if hour_of_day > 23 {
+    //     None
+    // } else if hour_of_day >= 22 {
+    //     Some(0)
+    // } else {
+    //     Some(5)
+    // }
+    
+    match hour_of_day {
+        0..=21 => Some(5),
+        22..=23 => Some(0),
+        24.. => None  
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+    // maybe_ice_cream(222).unwrap();
 }
 
 #[cfg(test)]
@@ -18,7 +32,7 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
-        let ice_creams = maybe_ice_cream(12);
+        let ice_creams = maybe_ice_cream(12).unwrap();
 
         assert_eq!(ice_creams, 5); // Don't change this line.
     }
--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,33 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        // let mut result = Vec::new();
+        // for (string, command) in input {
+        //     let modified_string = modified_string(string, command);
+        //     result.push(modified_string);
+        // };
+        // result
+        // input.into_iter().map(|(s, c)| modified_string(s, c)).collect()
+        input.into_iter().map(modified_string_2).collect()
+    }
+
+    // fn modified_string(string: String, command: Command) -> String {
+    //     match command {
+    //         Command::Uppercase => string.to_uppercase(),
+    //         Command::Trim => string.trim().to_string(),
+    //         Command::Append(i) => string + &"bar".repeat(i),
+    //     }
+    // }
+
+    fn modified_string_2(input: (String, Command)) -> String {
+        let (string, command) = input;
+        match command {
+            Command::Uppercase => string.to_uppercase(),
+            Command::Trim => string.trim().to_string(),
+            Command::Append(i) => string + &"bar".repeat(i),
+        }
+    }
 }
 
 fn main() {
@@ -38,6 +64,7 @@ fn main() {
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
