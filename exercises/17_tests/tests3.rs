d2d88c9602764daa2ee4f47e0225d88a4e9379cd
phil.markgraf <pmarkgraf@tandemdiabetes.com>
2026-07-24 11:40:41 -0700

    Work from Week 10

--- exercises/17_tests/tests3.rs
+++ exercises/17_tests/tests3.rs
@@ -6,13 +6,38 @@ struct Rectangle {
 impl Rectangle {
     // Don't change this function.
     fn new(width: i32, height: i32) -> Self {
-        if width <= 0 || height <= 0 {
-            // Returning a `Result` would be better here. But we want to learn
-            // how to test functions that can panic.
-            panic!("Rectangle width and height must be positive");
+        // Returning a `Result` would be better here. But we want to learn
+        // how to test functions that can panic.
+        // if width <= 0 && height <= 0 {
+        //     panic!("Rectangle width and height must be positive");
+        // } else if width <= 0 {
+        //     panic!("Rectangle width must be positive");
+        // } else if height <= 0 {
+        //     panic!("Rectangle height must be positive");
+        // } else {
+        //     Rectangle { width, height }
+        // }
+
+        // match (width, height) {
+        //     (..1, ..1) => panic!("Rectangle width and height must be positive"),
+        //     (..1, _) => panic!("Rectangle width must be positive"),
+        //     (_, ..1) => panic!("Rectangle height must be positive"),
+        //     (_, _) => Rectangle { width, height },
+        // }
+
+        match (width, height) {
+            (1.., 1..) => Rectangle { width, height },
+            (1.., _) => panic!("Rectangle height must be positive"),
+            (_, 1..) => panic!("Rectangle width must be positive"),
+            (_, _) => panic!("Rectangle width and height must be positive"),
         }
 
-        Rectangle { width, height }
+        // match (width > 0, height > 0) {
+        //     (true, true) => Rectangle { width, height },
+        //     (true, false) => panic!("Rectangle height must be positive"),
+        //     (false, true) => panic!("Rectangle width must be positive"),
+        //     (false, false) => panic!("Rectangle width and height must be positive"),
+        // }
     }
 }
 
@@ -29,21 +54,45 @@ mod tests {
         // TODO: This test should check if the rectangle has the size that we
         // pass to its constructor.
         let rect = Rectangle::new(10, 20);
-        assert_eq!(todo!(), 10); // Check width
-        assert_eq!(todo!(), 20); // Check height
+        assert_eq!(rect.width, 10); // Check width
+        assert_eq!(rect.height, 20); // Check height
     }
 
     // TODO: This test should check if the program panics when we try to create
     // a rectangle with negative width.
     #[test]
+    #[should_panic(expected = "Rectangle width must be positive")]
     fn negative_width() {
         let _rect = Rectangle::new(-10, 10);
     }
-
-    // TODO: This test should check if the program panics when we try to create
-    // a rectangle with negative height.
+    
     #[test]
+    #[should_panic(expected = "Rectangle width must be positive")]
+    fn zero_width() {
+        let _rect = Rectangle::new(0, 10);
+    }
+
+    #[test]
+    #[should_panic(expected = "Rectangle height must be positive")]
     fn negative_height() {
         let _rect = Rectangle::new(10, -10);
+    }
+
+    #[test]
+    #[should_panic(expected = "Rectangle height must be positive")]
+    fn zero_height() {
+        let _rect = Rectangle::new(10, 0);
+    }
+
+    #[test]
+    #[should_panic(expected = "Rectangle width and height must be positive")]
+    fn zero_both() {
+        let _rect = Rectangle::new(0, 0);
+    }
+
+    #[test]
+    #[should_panic(expected = "Rectangle width and height must be positive")]
+    fn negative_both() {
+        let _rect = Rectangle::new(-30, -10);
     }
 }
