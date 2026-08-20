bc55ca48dd7e09ea10759944c497ac885a275abd
phil.markgraf <pmarkgraf@tandemdiabetes.com>
2026-08-20 16:42:09 -0700

    Changes from week 14
    Co-authored-by: Brady King <tkingemb@users.noreply.github.com>
    Co-authored-by: John Boyd <jboyd@tandemdiabetes.com>

--- exercises/23_conversions/try_from_into.rs
+++ exercises/23_conversions/try_from_into.rs
@@ -28,14 +28,27 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
 
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        // let (r, g, b) = tuple;
+        // match (u8::try_from(r), u8::try_from(g), u8::try_from(b)) {
+        match (u8::try_from(tuple.0), u8::try_from(tuple.1), u8::try_from(tuple.2)) {
+            (Ok(red), Ok(green), Ok(blue)) => Ok(Color{red, green, blue}),
+            _ => Err(IntoColorError::IntConversion)
+        }
+    }
 }
 
 // TODO: Array implementation.
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let [r, g, b] = arr;
+        match (u8::try_from(r), u8::try_from(g), u8::try_from(b)) {
+            (Ok(red), Ok(green), Ok(blue)) => Ok(Color{red, green, blue}),
+            _ => Err(IntoColorError::IntConversion)
+        }
+    }
 }
 
 // TODO: Slice implementation.
@@ -43,7 +56,13 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        let arr: [i16; 3] = [slice[0], slice[1], slice[2]];
+        Color::try_from(arr)
+    }
 }
 
 fn main() {
